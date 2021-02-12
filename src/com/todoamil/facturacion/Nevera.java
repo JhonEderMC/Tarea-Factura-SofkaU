f9260c7492e404ab9feec131e6c08d78cf5467e2
jhon mc <jhoneder@hotmail.com>
2021-02-11 20:39:21 -0500

    agregar funcion calcular precio de la clase Nevera

---
+++ src/com/todoamil/facturacion/Nevera.java
@@ -0,0 +1,35 @@
+
+
+package com.todoamil.facturacion;
+
+public class Nevera extends Electrodomesticos {
+
+    private Integer capacidad;
+
+    public Nevera(String nombre, Character consumo, String procedencia, Integer capacidad) {
+        super(nombre, consumo, procedencia);
+        this.capacidad = capacidad;
+        precio();
+    }
+
+    /*
+        Calcular el precio de la nevera segun el numero de listros
+     */
+    @Override
+    public void precio(){
+        if (capacidad>120){
+            Integer listros= capacidad-120; //cantidad de listros demas que tiene
+            Integer cantidad = capacidad/10;  //veces demas a caborar
+            Double prec = getPrecio() + cantidad*getPrecio()*0.05;
+            setPrecio(prec);
+        }
+    }
+
+    public Integer getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Integer capacidad) {
+        this.capacidad = capacidad;
+    }
+}
